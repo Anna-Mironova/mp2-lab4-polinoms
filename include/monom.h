70de758b5a3dd1ea214d44786470f126ce21a4a6
Anna-Mironova <anyt.ka@bk.ru>
2015-12-14 22:12:33 +0300

    Частичная реализация монома

--- include/monom.h
+++ include/monom.h
@@ -1,1 +1,24 @@
 // класс или структура Монома (double коэффициент, целая свернутая степень)
+#ifndef __MONOM_H__
+#define __MONOM_H__
+
+#include <iostream>
+using namespace std;
+
+class Monom
+{
+	public:
+		double Coeff;	  // коэффициент монома
+		int Degree;       // степени переменных; 1 цифра - степень x; 2 цифра - степень y; 3 цифра - степень z
+
+		Monom (double c, int deg);	            //конструктор инициализатор
+		Monom (const Monom &m);                 //конструктор копирования
+		~Monom();                               //деструктор
+
+		Monom& operator=(const Monom &m);     // присваивание
+		
+		friend ostream& operator<<(ostream &out, const Monom &m); // вывод
+  
+};
+
+#endif
--- src/monom.cpp
+++ src/monom.cpp
@@ -1,1 +1,30 @@
 #include "monom.h"
+
+Monom::Monom(double c , int deg) //конструктор инициализатор
+{
+		Coeff=c;
+		Degree=deg;
+} 
+
+Monom::Monom(const Monom &m) //конструктор копирования
+{
+	Coeff=m.Coeff;
+	Degree=m.Degree;
+} 
+
+Monom::~Monom() //деструктор
+{}
+
+Monom& Monom::operator=(const Monom &m)// присваивание
+{
+	if(this == &m)
+		return *this;
+	Coeff=m.Coeff;
+	Degree=m.Degree;
+	return *this;
+} 
+std::ostream& operator<<(ostream &out, const Monom &m) //вывод
+  {
+	  out << '( ' << m.Coeff << ' , ' << m.Degree << ' )'<<endl;
+    return out;
+  }
