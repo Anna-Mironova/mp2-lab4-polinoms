c3692dc13dc3b42afb5e136800f9ef689dce39c0
Anna-Mironova <anyt.ka@bk.ru>
2015-12-14 22:49:38 +0300

    Начала реализацию полинома

--- include/polinom.h
+++ include/polinom.h
@@ -2,5 +2,26 @@
 // поддерживает операции Печать, Вставка монома, Удаление монома,  
 // Поиск монома, Сложение полиномов(слияние упорядоченных списков), 
 // Умножение полиномов
+#ifndef __POLINOM_H__
+#define __POLINOM_H__
 
 #include "monom.h"
+#include <iostream>
+using namespace std;
+
+class Polinom
+{
+	public:
+		Monom * Value;	     // Моном-данные одного звена
+		Polinom * Next;   // Указатель на следующий
+		Polinom * Head;   // Голова списка
+
+		Polinom ();	                                //конструктор инициализатор
+		Polinom (const Polinom &p);                 //конструктор копирования
+		~Polinom();                               //деструктор
+
+		
+		friend ostream& operator<<(ostream &out, const Polinom &p); // вывод
+  
+};
+#endif
--- src/polinom.cpp
+++ src/polinom.cpp
@@ -1,1 +1,29 @@
 #include "polinom.h"
+
+Polinom :: Polinom ()//конструктор инициализатор
+{
+	Next=NULL;
+}    
+
+Polinom :: Polinom (const Polinom &p) //конструктор копирования
+{
+	Head=p.Head;
+	Value=p.Value;
+	Next=p.Next;
+}
+
+Polinom :: ~Polinom() //деструктор
+{
+}
+
+std :: ostream& operator<<(ostream &out, const Polinom &p) // вывод
+{
+	Polinom * tmp = &p;
+	while(tmp!=NULL)
+	{
+		out << tmp->Value << ' + ';
+		tmp=tmp->Next;
+	}
+	out<< endl;
+	return out;
+}
